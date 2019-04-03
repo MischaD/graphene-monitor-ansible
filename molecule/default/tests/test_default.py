b26e0dcec8b38c751f5338dd7191e665d51ae29b
MischaD <mischa.dombrowski@gmail.com>
2019-04-03 09:36:23 +0200

    Added git repo for graphene monitor ansible

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,22 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_demo_file(host):
+    f = host.file('/tmp/demo.ini')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
